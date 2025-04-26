fd4509fa9807446e2bd8cb40545b31c67653c783
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2025-04-26 14:00:10 -0300

    fazendo testes de findOneById

---
+++ tests/routes/tweets/findOneById-tweet.test.ts
@@ -0,0 +1,82 @@
+import supertest from 'supertest';
+import { createExpressServer } from '../../../src/express.server';
+import { UserMock } from '../../mock/user.mock';
+import { makeToken } from '../make-token';
+import { TweetService } from "../../../src/services/tweet.service";
+import { randomUUID } from "crypto";
+
+describe('GET /tweets/:id', () => {
+	const server = createExpressServer();
+	const endpoint = '/tweets';
+	const userMock = UserMock.build();
+	const token = makeToken(userMock);
+
+	it('Deve retornar 400 quando o UUID for inválido', async () => {
+		const invalidId = 'abc';
+
+		const response = await supertest(server)
+			.get(`${endpoint}/${invalidId}`)
+			.set('Authorization', `Bearer ${token}`);
+
+		expect(response.status).toBe(400);
+		expect(response.body.success).toBe(false);
+		expect(response.body.message).toMatch(/uuid/i);
+	});
+
+	it('Deve retornar 403 quando o tweet não for encontrado', async () => {
+		const validId = randomUUID(); 
+
+		jest.spyOn(TweetService.prototype, 'findOneById').mockResolvedValue({
+			success: false,
+			code: 403,
+			message:
+				'Acesso negado: você não tem permissão para acessar este tweet !',
+		});
+
+		const response = await supertest(server)
+			.get(`${endpoint}/${validId}`)
+			.set('Authorization', `Bearer ${token}`);
+
+		expect(response.status).toBe(403);
+		expect(response.body.success).toBe(false);
+		expect(response.body.message).toMatch(/acesso negado/i);
+	});
+
+	it('Deve retornar 200 quando o tweet for encontrado', async () => {
+		const validId = randomUUID();
+
+		jest.spyOn(TweetService.prototype, 'findOneById').mockResolvedValue({
+			success: true,
+			code: 200,
+			message: 'Tweets buscados pelo ID com sucesso!',
+			data: { id: validId, content: 'teste', likeCount: 5 },
+		});
+
+		const response = await supertest(server)
+			.get(`${endpoint}/${validId}`)
+			.set('Authorization', `Bearer ${token}`);
+
+		expect(response.status).toBe(200);
+		expect(response.body.success).toBe(true);
+		expect(response.body.data).toHaveProperty('id', validId);
+		expect(response.body.data).toHaveProperty('likeCount');
+		expect(response.body.message).toMatch(/sucesso/i);
+	});
+
+	it('Deve retornar 500 quando houver erro no servidor', async () => {
+		const validId = randomUUID();
+
+		jest
+			.spyOn(TweetService.prototype, 'findOneById')
+			.mockRejectedValue(new Error('Falha interna'));
+
+		const response = await supertest(server)
+			.get(`${endpoint}/${validId}`)
+			.set('Authorization', `Bearer ${token}`);
+
+		expect(response.status).toBe(500);
+		expect(response.body.success).toBe(false);
+		expect(response.body.message).toMatch(/erro no servidor/i);
+	});
+
+});
