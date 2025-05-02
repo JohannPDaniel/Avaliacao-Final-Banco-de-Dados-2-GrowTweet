d188c4b56cf0a568d411d047fdcfbd08ce0f935f
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2025-05-02 15:33:25 -0300

    terminando teste de findAll de reply

--- src/middlewares/reply/find-all-reply.middleware.ts
+++ src/middlewares/reply/find-all-reply.middleware.ts
@@ -5,15 +5,12 @@ export class FindAllReplyMiddleware {
 	public static validateTypes(req: Request, res: Response, next: NextFunction) {
 		const { type } = req.query;
 
-		const isValidType =
-			type && Object.values(TypeTweetEnum).includes(type as TypeTweetEnum);
+		const isValidType = !type || type === TypeTweetEnum.Reply;
 
-		if (!isValidType && type) {
+		if (!isValidType) {
 			res.status(400).json({
 				success: false,
-				message: `Tipo inválido fornecido: ${type}. Os tipos válidos são ${Object.values(
-					TypeTweetEnum
-				).join(', ')}.`,
+				message: `Tipo inválido fornecido: ${type}. Apenas o tipo '${TypeTweetEnum.Reply}' é permitido.`,
 			});
 			return;
 		}
---
+++ tests/routes/replies/findAll-reply.test.ts
@@ -0,0 +1,80 @@
+import supertest from 'supertest';
+import { createExpressServer } from '../../../src/express.server';
+import { UserMock } from '../../mock';
+import { makeToken } from '../make-token';
+import { ReplyService } from '../../../src/services';
+
+describe('GET /replies', () => {
+	const server = createExpressServer();
+	const endpoint = '/replies';
+	const userMock = UserMock.build();
+	const token = makeToken(userMock);
+
+	it('Deve retornar 400 quando o tipo do tweet não for "REPLY"', async () => {
+		const query = { type: 'any_thing' };
+
+		const response = await supertest(server)
+			.get(endpoint)
+			.set('Authorization', `Bearer ${token}`)
+			.query(query);
+
+		expect(response.status).toBe(400);
+		expect(response.body.success).toBe(false);
+		expect(typeof response.body.message).toBe('string');
+		expect(response.body.message).toMatch(/inválido/i);
+		expect(Object.keys(response.body)).toContain('message');
+	});
+
+	it('Deve retornar 400 quando "type" for "Tweet"', async () => {
+		const query = { type: 'Tweet' };
+
+		const response = await supertest(server)
+			.get(endpoint)
+			.set('Authorization', `Bearer ${token}`)
+			.query(query);
+
+		expect(response.status).toBe(400);
+		expect(response.body.success).toBe(false);
+		expect(response.body.message).toMatch(/inválido/i);
+		expect(typeof response.body.message).toBe('string');
+		expect(Object.keys(response.body)).toContain('message');
+	});
+
+	it('Deve permitir pesquisar todos os Replies quando fornecido o tipo correto', async () => {
+		const query = { type: 'Reply' };
+
+		jest.spyOn(ReplyService.prototype, 'findAll').mockResolvedValue({
+			success: true,
+			code: 200,
+			message: 'Replies buscados com sucesso !',
+			data: {},
+		});
+
+		const response = await supertest(server)
+			.get(endpoint)
+			.set('Authorization', `Bearer ${token}`)
+			.query(query);
+
+		expect(response.status).toBe(200);
+		expect(response.body.success).toBe(true);
+		expect(response.body.message).toMatch(/sucesso/i);
+		expect(response.body).toHaveProperty('data');
+		expect(typeof response.body.message).toBe('string');
+	});
+
+	it('Deve retornar 500 quando houver um erro', async () => {
+		jest
+			.spyOn(ReplyService.prototype, 'findAll')
+			.mockRejectedValue(new Error('Exceção !!!'));
+
+		const response = await supertest(server)
+			.get(endpoint)
+			.set('Authorization', `Bearer ${token}`);
+
+		expect(response.statusCode).toBe(500);
+		expect(response.body.success).toBeFalsy();
+		expect(response.body.message).toMatch(/exceção/i);
+		expect(typeof response.body.message).toBe('string');
+		expect(Object.keys(response.body)).toContain('message');
+	});
+});
